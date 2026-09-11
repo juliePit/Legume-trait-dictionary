033a1c7c14b3e31ec487b8fc48a9c9ab82e47dce
juliePit <147841044+juliePit@users.noreply.github.com>
2026-09-11 14:50:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -200,8 +200,7 @@ for trait, group in grouped:
                     scale_class_series.iloc[0]
                 ).strip().lower()
                    
-                st.write("Available columns:")
-                st.write(list(mgroup.columns))
+               
                 if scale_class in ["ordinal", "nominal"]:
                     # display levels
     
